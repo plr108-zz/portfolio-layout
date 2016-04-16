d11d17a705e499b4b171504c041aff6ef9784cc6
Patrick Roche <patrick.l.roche@gmail.com>
2016-04-16 18:10:26 -0400

    Fix error related to opening project repo hyperlinks, fix typo in width var declaration

--- source/js/app.js
+++ source/js/app.js
@@ -46,7 +46,7 @@ var projects = [{
 var projectView = {
 
     setProjectImages: function() {
-        //var width = $(document).width();
+
         var smallBreakpoint1 = 400;
         var mediumBreakpoint1 = 500;
         var largeBreakpoint1 = 560;
@@ -56,7 +56,7 @@ var projectView = {
         var largeBreakpoint2 = 1104;
         var imageMode = 0;
 
-        var width = width = Math.max( $(window).width(), window.innerWidth);
+        var width = Math.max($(window).width(), window.innerWidth);
 
         if (width >= 1104) {
             imageMode = 3;
@@ -115,43 +115,54 @@ var projectView = {
         });
 
         $("#project-1").click(function() {
-            window.open("http://plr108.github.io/ballpark-map/index.html", "_blank");
-
+            if (!($(this).find(".project-repo").is(":hover"))) {
+                window.open("http://plr108.github.io/ballpark-map/index.html", "_blank");
+            }
         });
 
         $("#project-2").click(function() {
-            window.open("http://plr108.github.io/feed-the-ladybugs/index.html", "_blank");
-
+            if (!($(this).find(".project-repo").is(":hover"))) {
+                window.open("http://plr108.github.io/feed-the-ladybugs/index.html", "_blank");
+            }
         });
 
         $("#project-3").click(function() {
-            window.open("http://plr108.github.io/resume/index.html", "_blank");
-
+            if (!($(this).find(".project-repo").is(":hover"))) {
+                window.open("http://plr108.github.io/resume/index.html", "_blank");
+            }
         });
 
         $("#project-4").click(function() {
-            window.open("http://plr108.github.io/web-perf/index.html", "_blank");
-
+            if (!($(this).find(".project-repo").is(":hover"))) {
+                window.open("http://plr108.github.io/web-perf/index.html", "_blank");
+            }
         });
 
         $("#project-5").click(function() {
-            window.open("http://plr108.github.io/mccsings/index.html", "_blank");
+            if (!($(this).find(".project-repo").is(":hover"))) {
+                window.open("http://plr108.github.io/mccsings/index.html", "_blank");
+            }
         });
 
         $("#project-6").click(function() {
-            window.open("http://plr108.github.io/jasmine-testing/index.html", "_blank");
-
+            if (!($(this).find(".project-repo").is(":hover"))) {
+                window.open("http://plr108.github.io/jasmine-testing/index.html", "_blank");
+            }
         });
 
         $(".project").hover(function() {
-                $(this).find(".project-title").css("text-decoration", "underline");
                 var titleDiv = $(this).find(".project-title");
-                $(this).find(".project-description").css("text-decoration", "underline");
                 var descriptionDiv = $(this).find(".project-description");
+                var repoDiv = $(this).find(".project-repo");
 
-                $(this).find(".project-repo").hover(function() {
+                titleDiv.css("text-decoration", "underline");
+                descriptionDiv.css("text-decoration", "underline");
+
+
+                repoDiv.hover(function() {
                         titleDiv.css("text-decoration", "none");
                         descriptionDiv.css("text-decoration", "none");
+
                     },
                     function() {
                         titleDiv.css("text-decoration", "underline");
@@ -165,39 +176,27 @@ var projectView = {
 
 
         $("#project-1-repo").click(function() {
-            if ($(this).find("a").length) {
-                window.open("https://github.com/plr108/ballpark-map", "_blank");
-            }
+            window.open("https://github.com/plr108/ballpark-map", "_blank");
         });
 
         $("#project-2-repo").click(function() {
-            if ($(this).find("a").length) {
-                window.open("https://github.com/plr108/feed-the-ladybugs", "_blank");
-            }
+            window.open("https://github.com/plr108/feed-the-ladybugs", "_blank");
         });
 
         $("#project-3-repo").click(function() {
-            if ($(this).find("a").length) {
-                window.open("https://github.com/plr108/resume", "_blank");
-            }
+            window.open("https://github.com/plr108/resume", "_blank");
         });
 
         $("#project-4-repo").click(function() {
-            if ($(this).find("a").length) {
-                window.open("https://github.com/plr108/web-perf", "_blank");
-            }
+            window.open("https://github.com/plr108/web-perf", "_blank");
         });
 
         $("#project-5-repo").click(function() {
-            if ($(this).find("a").length) {
-                window.open("https://github.com/plr108/mccsings", "_blank");
-            }
+            window.open("https://github.com/plr108/mccsings", "_blank");
         });
 
         $("#project-6-repo").click(function() {
-            if ($(this).find("a").length) {
-                window.open("https://github.com/plr108/jasmine-testing", "_blank");
-            }
+            window.open("https://github.com/plr108/jasmine-testing", "_blank");
         });
     }
 }
